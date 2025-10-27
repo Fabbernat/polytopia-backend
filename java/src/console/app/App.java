086fe1b4360351d2662a96871e40366140c6613a
Fabian Bernat <fabbernat@gmail.com>
2025-10-27 17:37:09 +0100

    v1.4: Fixed a lot of bugs in reading in guidReference.txt and games.txt, no errors or exeptions thrown in this version

--- java/src/console/app/App.java
+++ java/src/console/app/App.java
@@ -3,10 +3,13 @@ package console.app;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 import static console.Main.log;
@@ -14,18 +17,36 @@ import static console.Main.log;
 public class App {
   static int guid = 1;
   List<String> games = new ArrayList<>();
-  public App(){
+
+  public App() {
     readGuidReferenceDotTxt();
     readGamesDotTxt();
   }
 
 
   private void readGuidReferenceDotTxt() {
-    try (Scanner scanner = new Scanner(new File("guidReference.txt"))) {
+    try (InputStream input = App.class.getResourceAsStream("/guidReference.txt")) {
+      if (input == null) {
+        log("guidReference.txt not found!");
+        return;
+      }
+      List<String> lines = Files.readAllLines(
+              Paths.get(Objects.requireNonNull(App.class.getResource("/guidReference.txt")).toURI())
+      );
+      log("Read in guid reference: ");
+      lines.forEach(System.out::println);
+      try {
+        guid = Integer.parseInt(lines.getFirst());
+      } catch (NumberFormatException numberFormatException) {
+        numberFormatException.printStackTrace();
+      }
+    } catch (IOException | URISyntaxException exception) {
+      exception.printStackTrace();
+    }
+    /*try (Scanner scanner = new Scanner(new File("/guidReference.txt"))) {
       if (scanner.hasNextLine()) {
+        String line = scanner.nextLine();
         try {
-          String line = scanner.nextLine();
-          log("Read in guid reference: " + line);
           guid = Integer.parseInt(line);
         } catch (NumberFormatException numberFormatException) {
           numberFormatException.printStackTrace();
@@ -33,20 +54,34 @@ public class App {
       }
     } catch (FileNotFoundException fileNotFoundException) {
       fileNotFoundException.printStackTrace();
-    }
+    }*/
   }
 
   private void readGamesDotTxt() {
-    try {
-      List<String> lines = Files.readAllLines(Paths.get("games.txt"));
+    try (InputStream input = App.class.getResourceAsStream("/games.txt")) {
+      if (input == null) {
+        log("games.txt not found!");
+        return;
+      }
+      List<String> lines = Files.readAllLines(
+              Paths.get(Objects.requireNonNull(App.class.getResource("/games.txt")).toURI())
+      );
       log("Read in games: ");
+      lines.forEach(System.out::println);
+      games.addAll(lines);
+    } catch (IOException | URISyntaxException ioException) {
+      ioException.printStackTrace();
+    }
+
+    /*try {
+      List<String> lines = Files.readAllLines(Paths.get("/games.txt"));
       for (String line : lines) {
         log(line);
       }
       games.addAll(lines);
     } catch (IOException e) {
       e.printStackTrace();
-    }
+    }*/
   }
 
 }

910e63bdba5068ee4d230eed13a696357b7446b1
fabbernatvasvari <sze4fabber@vasvari.org>
2026-01-15 16:06:33 +0100

    Fix games bug

--- java/src/console/app/App.java
+++ java/src/console/app/App.java
@@ -1,7 +1,5 @@
 package console.app;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
@@ -10,7 +8,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.Scanner;
 
 import static console.Main.log;
 
@@ -19,12 +16,12 @@ public class App {
   static List<String> games = new ArrayList<>();
 
   public App() {
-    readGuidReferenceDotTxt();
-    readGamesDotTxt();
+    readGuidReference();
+    readGames();
   }
 
 
-  private void readGuidReferenceDotTxt() {
+  private void readGuidReference() {
     try (InputStream input = App.class.getResourceAsStream("/guidReference.txt")) {
       if (input == null) {
         log("guidReference.txt not found!");
@@ -43,49 +40,31 @@ public class App {
     } catch (IOException | URISyntaxException exception) {
       exception.printStackTrace();
     }
-    /*try (Scanner scanner = new Scanner(new File("/guidReference.txt"))) {
-      if (scanner.hasNextLine()) {
-        String line = scanner.nextLine();
-        try {
-          guid = Integer.parseInt(line);
-        } catch (NumberFormatException numberFormatException) {
-          numberFormatException.printStackTrace();
-        }
-      }
-    } catch (FileNotFoundException fileNotFoundException) {
-      fileNotFoundException.printStackTrace();
-    }*/
   }
 
-  private void readGamesDotTxt() {
-    try (InputStream input = App.class.getResourceAsStream("/games.txt")) {
-      if (input == null) {
-        log("games.txt not found!");
-        return;
-      }
-      List<String> lines = Files.readAllLines(
-              Paths.get(Objects.requireNonNull(App.class.getResource("/games.txt")).toURI())
-      );
-      log("Read in games: ");
+    private void readGames() {
+        try (InputStream input = App.class.getResourceAsStream("/games.txt")) {
+            if (input == null) {
+                log("games.txt not found!");
+                return;
+            }
 
-      games.addAll(lines);
-      reportGames();
-    } catch (IOException | URISyntaxException ioException) {
-      ioException.printStackTrace();
+            List<String> lines = new java.io.BufferedReader(
+                    new java.io.InputStreamReader(input)
+            ).lines().toList();
+
+            log("Read in games:");
+            games.clear();
+            games.addAll(lines);
+            reportGames();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    /*try {
-      List<String> lines = Files.readAllLines(Paths.get("/games.txt"));
-      for (String line : lines) {
-        log(line);
-      }
-      games.addAll(lines);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }*/
-  }
 
-  public static void reportGames() {
+    public static void reportGames() {
     games.forEach(System.out::println);
   }
 
